94d971b8682e04d799cdf4670834ad7c0726c95b
kali <kali@kali.tamil>
2025-04-05 23:28:38 +0530

    Add GitHub Actions workflow

---
+++ app/server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const PORT = 80;
+const server = http.createServer((req, res) => {
+  res.end('Hello from Kubernetes Node.js App!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
